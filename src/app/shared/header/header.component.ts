b094ef6be9f49912a341726a0f7a5f6e2dfc6d78
Gabriel Toth Gonçalves <60109701+GabrielToth@users.noreply.github.com>
2022-11-21 18:00:54 -0300

    Terminando de fazer a "Soluções" funcionar corretamente
    Para finalizar este problema é necessário terminar duas etapas:
    1. Arrumar a função que, ao retirar o mouse de cima do botão soluções E não colocar em cima da div soluções, remova a classe da div.
    2. Indexar os botões de cada sessão.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -9,6 +9,20 @@ export class HeaderComponent {
 
   display: boolean = false;
 
+  solution = document.getElementById('solution');
+
+  onMouseLeaveSolution() {
+    setTimeout(this.verifyMouse(), 5000);
+  }
+
+  verifyMouse() {
+      if (this.display === true) {
+        return 'a'
+      } else {
+        return 'b'
+      }
+  };
+
   onMouseEnter() {
     this.display = true;
   }
