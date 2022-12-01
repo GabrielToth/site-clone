ef1e76e62ae44108c2f4e143f5a9f8cd106dacc9
Gustavo-AB <g.alvarez.b12@gmail.com>
2022-12-01 13:39:31 -0300

    refatorei o css da pag suporte para scss e terminei o responsivo

--- src/app/main/support/support.component.ts
+++ src/app/main/support/support.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { TopPageContentTitleService } from '../top-page-content-title.service';
 
 @Component({
   selector: 'app-support',
   templateUrl: './support.component.html',
   styleUrls: ['./support.component.scss']
 })
-export class SupportComponent implements OnInit {
+export class SupportComponent {
 
-  constructor() { }
+  title?:string;
 
-  ngOnInit(): void {
+  constructor(private Title:TopPageContentTitleService){
+    this.title = Title.getTitle("Suporte")
   }
 
 }
