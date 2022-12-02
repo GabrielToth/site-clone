b5d6613ed8cd1e7635af81d1dc13009eb14f0c39
Gabriel Toth Gonçalves <60109701+GabrielToth@users.noreply.github.com>
2022-12-02 14:35:11 -0300

    att

---
+++ src/app/shared/headline/headline.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headline',
+  templateUrl: './headline.component.html',
+  styleUrls: ['./headline.component.scss']
+})
+export class HeadlineComponent implements OnInit {
+  colorr: any; 
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.colorr = document.getElementsByClassName('background-color').item(0)?.innerHTML;
+  }
+
+}
