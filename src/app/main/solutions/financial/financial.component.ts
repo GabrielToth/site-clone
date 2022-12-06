4a3625cacef290f3479df9865d048e2ddffc83de
Gabriel Toth Gonçalves <60109701+GabrielToth@users.noreply.github.com>
2022-12-06 17:58:25 -0300

    ts clean

--- src/app/main/solutions/financial/financial.component.ts
+++ src/app/main/solutions/financial/financial.component.ts
@@ -1,17 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { TopPageContentTitleService } from '../../top-page-content-title.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-financial',
   templateUrl: './financial.component.html',
   styleUrls: ['./financial.component.scss']
 })
-export class FinancialComponent {
-
-  title?:string;
-
-  constructor(private Title:TopPageContentTitleService){
-    this.title = Title.getTitle("Financeiro")
-  }
-
-}
+export class FinancialComponent {}
