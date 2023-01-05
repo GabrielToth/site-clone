524b72462510324690c7c591634f4637f0ee2ed7
Gustavo Alvarez <72941004+Gustavo-AB@users.noreply.github.com>
2023-01-05 10:22:13 -0300

    coloquei a func de envio de email

--- src/app/main/contact/forms/forms.component.ts
+++ src/app/main/contact/forms/forms.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Client } from './client/client';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss']
+  styleUrls: ['./forms.component.scss'],
 })
 export class FormsComponent implements OnInit {
   formClient!: FormGroup;
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.createForm(new Client());
@@ -22,12 +21,28 @@ export class FormsComponent implements OnInit {
       enterprise: new FormControl(client.enterprise),
       email: new FormControl(client.email),
       phone: new FormControl(client.phone, Validators.pattern('[- +()0-9]+')),
-      message: new FormControl(client.message)
+      message: new FormControl(client.message),
     });
   }
 
   onSubmit(): void {
-     console.log(this.formClient.value);
-  }
+    const { name } = this.formClient.value;
+    const { enterprise } = this.formClient.value;
+    const { email } = this.formClient.value;
+    const { phone } = this.formClient.value;
+    const { message } = this.formClient.value;
 
+    const sub = `Suporte | ${name} - ${enterprise}`;
+    const body = `
+    Nome: ${name}\n
+    Companhia: ${enterprise}\n
+    Email: ${email}\n
+    Telefone: ${phone}\n
+    Mensagem: ${message}\n
+	`;
+
+    window.open(
+      `mailto:softclever@softclever.com.br?subject=${sub}&body=${body}`
+    );
+  }
 }
